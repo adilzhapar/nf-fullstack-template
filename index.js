accaab3dce295aeeed5a7ac99bc874e90467004d
Adil Zhapar <adilzhapar2002@gmail.com>
2023-09-21 11:24:34 +0600

    initial-2

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-let contentElement = document.getElementById('content');
 
-document.getElementById('characters').addEventListener('click', function () {
-    axios.get('https://rickandmortyapi.com/api/character')
-        .then(function (response) {
-
-            console.log(response);
-            const apiData = response.data.results;
-            const apiDataElement = document.getElementById('api-data');
-
-            apiData.forEach(function (item) {
-                const div = document.createElement('div');
-                div.className = 'item-card';
-                div.innerHTML = `
-                <img src=${item.image}></img>
-                <strong>${item.name}</strong>
-                `
-
-                apiDataElement.appendChild(div);
-            });
-
-        })
-        .catch(function (error) {
-            console.error('Error fetching data:', error);
-        });
-
-})
 
 
